f2270b8569b62a371084ca7afa949451e3435a92
unknown <viakeegan@gmail.com>
2022-02-03 02:00:37 -0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -17,20 +17,25 @@ const server = new ApolloServer({
 });
 
 // integrate apollo server with express app as middleware
-server.applyMiddleware({ app });
+async function wait (){
+  await server.start()
+  server.applyMiddleware({ app });;
+}
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+// app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// })
 
 db.once('open', () => {
   app.listen(PORT, () => {
