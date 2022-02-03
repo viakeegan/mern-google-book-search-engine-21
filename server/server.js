8bc8b55d560d4757e02ea614fa8d24826fcc2b6d
unknown <viakeegan@gmail.com>
2022-02-03 02:16:01 -0500

    refractor

--- server/server.js
+++ server/server.js
@@ -24,18 +24,19 @@ async function wait (){
 wait()
 
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+//if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.use(routes);
-// app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// })
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
